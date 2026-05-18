46fb335b2001de1311b9d95555d3cbe057c2e69d
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-18 14:10:34 +0500

    (feat): добавлен базовый worker

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
 import os
 
-RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://admin:admin@rabbitmq:5672/")
 QUEUE_NAME = "analyze_queue"
--- backend/app/workers/main.py
+++ backend/app/workers/main.py
@@ -1,1 +1,38 @@
-# TODO
+import aio_pika
+import logging
+import asyncio
+
+from config import RABBITMQ_URL, QUEUE_NAME
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("worker")
+
+async def handle_message(message: aio_pika.IncomingMessage) -> None:
+    """
+    TODO: Обрабатывает сообщение из очереди
+    """
+
+    logger.info(f"[*] Received {message}")
+
+async def main() -> None:
+    """
+    Подключается к RabbitMQ и слушает очередь
+    """
+
+    connection = await aio_pika.connect(RABBITMQ_URL)
+
+    async with connection:
+        channel = await connection.channel()
+        await channel.set_qos(prefetch_count=1) # Ровно одна задача
+        queue = await channel.declare_queue(QUEUE_NAME, durable=True)
+
+        logger.info(f"[*] Waiting for messages from {QUEUE_NAME}...")
+
+        async with queue.iterator() as iterator:
+            async for message in iterator:
+                async with message.process(requeue=True):
+                    await handle_message(message)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
